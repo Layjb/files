e67c73310722f2ec9e541f7ed9341d8bc5e63b87
M09Ic <m09ician@gmail.com>
2022-07-05 18:21:32 +0800

    add getexecpath func

--- file.go
+++ file.go
@@ -5,6 +5,9 @@ import (
 	"bytes"
 	"errors"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func isExist(filename string) bool {
@@ -134,4 +137,14 @@ func (f *File) Close() {
 	}
 	f.Sync()
 	_ = f.FileHandler.Close()
+}
+
+func GetExcPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+	// 获取包含可执行文件名称的路径
+	path, _ := filepath.Abs(file)
+	// 获取可执行文件所在目录
+	index := strings.LastIndex(path, string(os.PathSeparator))
+	ret := path[:index]
+	return strings.Replace(ret, "\\", "/", -1) + "/"
 }
