ace38c0df4bbf625a51c34bba3c5ae76e824962d
M09Ic <m09ician@gmail.com>
2022-07-05 17:53:14 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chainreactors/files
+
+go 1.17
---
+++ obfuscate.go
@@ -0,0 +1,29 @@
+package pkg
+
+import (
+	"bytes"
+	"compress/flate"
+	"io/ioutil"
+)
+
+func Flate(input []byte) []byte {
+	var bf = bytes.NewBuffer([]byte{})
+	var flater, _ = flate.NewWriter(bf, flate.BestCompression)
+	defer flater.Close()
+	if _, err := flater.Write(input); err != nil {
+		println(err.Error())
+		return []byte{}
+	}
+	if err := flater.Flush(); err != nil {
+		println(err.Error())
+		return []byte{}
+	}
+	return bf.Bytes()
+}
+
+func UnFlate(input []byte) []byte {
+	rdata := bytes.NewReader(input)
+	r := flate.NewReader(rdata)
+	s, _ := ioutil.ReadAll(r)
+	return s
+}
