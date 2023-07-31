acee21c8c45afdf2cb65957bc799678996676e7d
M09Ic <m09ician@gmail.com>
2023-08-01 01:48:53 +0800

    add HasStdin api

--- utils.go
+++ utils.go
@@ -156,3 +156,15 @@ func shannonEntropy(data []byte) float64 {
 
 	return entropy
 }
+
+func HasStdin() bool {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+
+	isPipedFromChrDev := (stat.Mode() & os.ModeCharDevice) == 0
+	isPipedFromFIFO := (stat.Mode() & os.ModeNamedPipe) != 0
+
+	return isPipedFromChrDev || isPipedFromFIFO
+}
